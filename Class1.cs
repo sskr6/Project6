1ada7177fb846c807975c4336415214e70a39c73
sskr <samashivakumarreddy@gmail.com>
2016-07-11 17:09:09 -0400

    added two files

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f, g,h;
+            a = 10;
+             b = 12;
+             c = ++a;
+             d = a + c;
+             e = a - b;
+             f = a / b;
+             g = a * b; 
+             h = a % b;
+            Console.WriteLine("value of a is {0}", a);
+            Console.WriteLine("value of b is {0}", b);
+            Console.WriteLine("value of c is {0}", c);
+            Console.WriteLine("value of d is {0}", d); 
+            Console.WriteLine("value of e is {0}", e);
+            Console.WriteLine("value of f is {0}", f);
+            Console.WriteLine("value of g is {0}", g);
+            Console.WriteLine("value of h is {0}", h); 
+            
+           
+            Console.ReadLine();
+           
+        }
+    }
+}
