a1d385fea1d8120b627e88e4d8b7e6fda743e02a
sskr <samashivakumarreddy@gmail.com>
2016-07-11 17:21:26 -0400

    added files

--- Class1.cs
+++ Class1.cs
@@ -4,33 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace basic
+namespace ClassLibrary2
 {
-    class Class1
+    public class Class1
     {
-        static void Main(string[] args)
+        public int add(int a ,int b)
         {
-            int a, b, c, d, e, f, g,h;
-            a = 10;
-             b = 12;
-             c = ++a;
-             d = a + c;
-             e = a - b;
-             f = a / b;
-             g = a * b; 
-             h = a % b;
-            Console.WriteLine("value of a is {0}", a);
-            Console.WriteLine("value of b is {0}", b);
-            Console.WriteLine("value of c is {0}", c);
-            Console.WriteLine("value of d is {0}", d); 
-            Console.WriteLine("value of e is {0}", e);
-            Console.WriteLine("value of f is {0}", f);
-            Console.WriteLine("value of g is {0}", g);
-            Console.WriteLine("value of h is {0}", h); 
-            
-           
-            Console.ReadLine();
-           
+            return a + b;
+        }
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int mul(int a, int b)
+        {
+            return a * b;
         }
     }
 }
